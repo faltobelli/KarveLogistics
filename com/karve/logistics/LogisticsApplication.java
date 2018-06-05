baa1774862b59f479405b8f5b2b39802abf2bb70
frank altobelli <frankaltobelli@gmail.com>
2018-06-04 23:06:02 -0500

    try new azure setup

--- com/karve/logistics/LogisticsApplication.java
+++ com/karve/logistics/LogisticsApplication.java
@@ -1,12 +1,20 @@
 package com.karve.logistics;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LogisticsApplication {
+public class LogisticsApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(LogisticsApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
 }
